95ea118d50feec9c0c9bafddf591d6fbefcdac19
王志军 <hw_developer@163.com>
2018-03-01 15:05:14 +0800

    更新代码

---
+++ 00server/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+var bodyParser = require('body-parser');
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/get', function (req, res) {
+    var user={
+        name:'king',
+        age:'18'
+    }
+    res.write(JSON.stringify(user));
+    res.end();
+})
+
+app.post('/post', urlencodedParser, function (req, res) {
+    var name =req.body.name;
+    var age =req.body.age;
+    var user={
+        name:'超级'+name,
+        age:'超级'+age
+    }
+    res.write(JSON.stringify(user));
+    res.end();
+})
+
+var server = app.listen(8088)
